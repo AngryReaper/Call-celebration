60fdafadfb050a8bc07894aa2e02a37faf444043
AngryReaper <angryreaper@gmail.com>
2019-05-11 17:32:11 +0300

    image popup fixes

--- src/components/FrameModal.jsx
+++ src/components/FrameModal.jsx
@@ -2,8 +2,17 @@ import React, {Component} from 'react';
 import Modal from 'react-modal';
 
 class FrameModal extends Component {
+  constructor (props) {
+    super(props);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
+
   componentWillMount() {
     Modal.setAppElement('body');
+  }
+
+  handleCloseModal () {
+    this.props.handleClearImageSelect()
   }
 
   render() {
@@ -11,6 +20,7 @@ class FrameModal extends Component {
       <Modal
         isOpen={!!this.props.selectedPhoto}
         className="popup-modal"
+        onRequestClose={this.handleCloseModal}
       >
         {!!this.props.selectedPhoto && <div className="popup-modal-image-wrapper">
           <img src={require('../images/galleries/gallery'+this.props.currentGallery+'/'+this.props.selectedPhoto)} alt=""/>
