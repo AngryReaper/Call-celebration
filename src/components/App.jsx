ce42003088f6565d0f7bf1e286f9e445fe9541d3
AngryReaper <angryreaper@gmail.com>
2019-05-16 15:55:53 +0300

    loader fixes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component, Suspense, lazy } from 'react';
 import MainInfo from './MainInfo';
 import AppState from '../AppState';
+import loader from '../images/loader.gif';
 const Events = lazy(() => import('./Events'));
 const Gallery = lazy(() => import('./Gallery'));
 const Recalls = lazy(() => import('./Recalls'));
@@ -13,20 +14,20 @@ class App extends Component {
   render() {
     return (
       <div className="main-wrapper">
-        <h1 className="main-header">Я умею делать праздник!!</h1>
+        <h1 className="main-header">Я умею делать праздник!</h1>
         <h2 className="sub-header">Проведение свадеб, юбилеев, тематических праздников</h2>
         <div className="container">
           <MainInfo appState={appState} />
-          <Suspense fallback={<div>Loading...</div>}>
+          <Suspense fallback={<div className="loader"><img src={loader} alt=""/></div>}>
             <Events appState={appState} />
           </Suspense>
-          <Suspense fallback={<div>Loading...</div>}>
+          <Suspense fallback={<div className="loader"><img src={loader} alt=""/></div>}>
             <Gallery appState={appState} />
           </Suspense>
-          <Suspense fallback={<div>Loading...</div>}>
+          <Suspense fallback={<div className="loader"><img src={loader} alt=""/></div>}>
             <Recalls appState={appState} />
           </Suspense>
-          <Suspense fallback={<div>Loading...</div>}>
+          <Suspense fallback={<div className="loader"><img src={loader} alt=""/></div>}>
             <Contacts appState={appState} />
           </Suspense>
         </div>
